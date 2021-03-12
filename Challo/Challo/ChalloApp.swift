87c8ff7de97b74c9afa15372b2a307dad2056b6f
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2021-03-12 19:36:38 +0800

    Set up XCode project

---
+++ Challo/Challo/ChalloApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChalloApp.swift
+//  Challo
+//
+//  Created by Tan Le Yang on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct ChalloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challo/Challo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Challo
+//
+//  Created by Tan Le Yang on 12/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
