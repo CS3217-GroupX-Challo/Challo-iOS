c388f948d55fc6e20fe4454e9ca36c91dd0ba364
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2021-03-19 12:46:18 +0800

    Refactor user api response handler

--- Challo/Challo/Modules/Common/UserManagement/UserAPIInteractor.swift
+++ Challo/Challo/Modules/Common/UserManagement/UserAPIInteractor.swift
@@ -43,25 +43,36 @@ extension UserAPIInteractor {
     }
 
     func sendUserPostRequest(url: String,
-                         body: JSON,
-                         callback: @escaping (UserAPIResponse) -> Void) {
+                             body: JSON,
+                             callback: @escaping (UserAPIResponse) -> Void) {
         networkManager.post(url: url,
                             headers: AlamofireManager.HEADER(),
                             body: body) { res, err in
             if let err = err {
-                ChalloLogger.logger.log("Error: \(err as NSObject)")
-                let failureResponse = UserAPIResponse(success: false, error: err)
-                callback(failureResponse)
+                handleErrorResponse(error: err, callback: callback)
                 return
             }
-            guard let certificate = self.parseUser(apiResponse: res) else {
-                ChalloLogger.logger.log("Unable to parse api response \(res)")
-                let failureResponse = UserAPIResponse(success: false)
-                callback(failureResponse)
-                return
-            }
-            let successResponse = UserAPIResponse(success: true, certificate: certificate)
-            callback(successResponse)
+            handleSuccessResponse(response: res, callback: callback)
         }
+    }
+
+    private func handleErrorResponse(error: Error,
+                                     callback: @escaping (UserAPIResponse) -> Void) {
+        ChalloLogger.logger.log("Error: \(error as NSObject)")
+        let failureResponse = UserAPIResponse(success: false, error: error)
+        callback(failureResponse)
+        return
+    }
+
+    private func handleSuccessResponse(response: JSON,
+                                       callback: @escaping (UserAPIResponse) -> Void) {
+        guard let certificate = self.parseUser(apiResponse: response) else {
+            ChalloLogger.logger.log("Unable to parse api response \(response)")
+            let failureResponse = UserAPIResponse(success: false)
+            callback(failureResponse)
+            return
+        }
+        let successResponse = UserAPIResponse(success: true, certificate: certificate)
+        callback(successResponse)
     }
 }
