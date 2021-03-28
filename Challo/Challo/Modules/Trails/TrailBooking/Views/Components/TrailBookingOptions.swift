af40da0be3577ab3a888b1f575fd3f5fcae60268
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2021-03-28 14:18:18 +0800

    Add pricing to booking options

--- Challo/Challo/Modules/Trails/TrailBooking/Views/Components/TrailBookingOptions.swift
+++ Challo/Challo/Modules/Trails/TrailBooking/Views/Components/TrailBookingOptions.swift
@@ -9,14 +9,20 @@ import SwiftUI
 
 struct TrailBookingOptions: View {
 
+    @State private var pricePerPax = 25.5
     @State private var selectedPax = 0
     @State private var paxRange = [1, 2, 3, 4, 5]
 
     var body: some View {
-        Form {
-            TrailPaxSelection(selectedPax: $selectedPax,
-                              paxRange: $paxRange)
-            Text("Total cost: ")
+        VStack(alignment: .leading) {
+            Text("Price per guest: \(pricePerPax)")
+                .font(.title3)
+            Form {
+                TrailPaxSelection(selectedPax: $selectedPax,
+                                  paxRange: $paxRange)
+            }
+            Text("Total cost: \(pricePerPax * Double(selectedPax))")
+                .font(.title3)
         }
         .padding()
     }
