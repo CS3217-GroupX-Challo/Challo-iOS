19986d01e2d95d8dd95c054732733c2484514758
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2021-04-03 15:25:25 +0800

    Add iternaries sidebar

---
+++ Challo/Challo/Modules/Map/InteractiveMap/Views/Components/InteractiveMapSidebar.swift
@@ -0,0 +1,25 @@
+//
+//  InteractiveMapSidebar.swift
+//  Challo
+//
+//  Created by Tan Le Yang on 3/4/21.
+//
+
+import SwiftUI
+
+struct InteractiveMapSidebar: View {
+    
+    @EnvironmentObject presenter: MapPresenter
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .edgesIgnoringSafeArea(.all)
+            .navigationBarTitle(Text("Itineraries"), displayMode: .inline)
+    }
+}
+
+struct InteractiveMapSidebar_Previews: PreviewProvider {
+    static var previews: some View {
+        InteractiveMapSidebar()
+    }
+}
---
+++ Challo/Challo/Modules/Map/InteractiveMap/Views/Components/InteractiveMapTopBar.swift
@@ -0,0 +1,33 @@
+//
+//  InteractiveMapTopBar.swift
+//  Challo
+//
+//  Created by Tan Le Yang on 3/4/21.
+//
+
+import SwiftUI
+
+struct InteractiveMapTopBar: View {
+
+    @EnvironmentObject var presenter: MapPresenter
+
+    var body: some View {
+        HStack {
+            TextField("Search...",
+                      text: $presenter.searchQuery,
+                      onCommit: {
+                        presenter.findPlaces()
+                      })
+                .textFieldStyle(RoundedTextFieldStyle())
+                .padding()
+                .autocapitalization(.none)
+                .zIndex(1)
+        }
+    }
+}
+
+struct InteractiveMapTopBar_Previews: PreviewProvider {
+    static var previews: some View {
+        InteractiveMapTopBar()
+    }
+}
