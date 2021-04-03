7e30c7b234cd01269c438eae964d5d5be5b7b0bc
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2021-04-03 15:44:48 +0800

    Add ItineraryListingCard

--- Challo/Challo/Modules/Map/InteractiveMap/Views/Components/InteractiveMapSidebar.swift
+++ Challo/Challo/Modules/Map/InteractiveMap/Views/Components/InteractiveMapSidebar.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct InteractiveMapSidebar: View {
     
-    @EnvironmentObject presenter: MapPresenter
+    @EnvironmentObject var presenter: MapPresenter
 
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
