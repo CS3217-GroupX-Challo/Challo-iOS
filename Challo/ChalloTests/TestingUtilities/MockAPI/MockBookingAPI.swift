aaac6e59ed66b716412eade65e53e362f83a9dd6
Ong Ying Gao <ong.yinggao@gmail.com>
2021-04-04 01:32:25 +0800

    Fix missing curly brace from conflict resolution

--- Challo/ChalloTests/TestingUtilities/MockAPI/MockBookingAPI.swift
+++ Challo/ChalloTests/TestingUtilities/MockAPI/MockBookingAPI.swift
@@ -30,8 +30,10 @@ class MockBookingAPI: BookingAPI {
 
     override func getBookingsForGuide(id: UUID, callback: @escaping ([Booking]) -> Void) {
         applyToBookings(callback: callback)
+    }
  
     override func makeBooking(bookingDetails: BookingDetails, callback: @escaping ((Bool, Error?) -> Void)) {
         callback(true, nil)
     }
+
 }
