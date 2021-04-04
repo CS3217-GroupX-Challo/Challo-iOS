beebb4ea211bb6aaffb4bd466567d8b2ee8ac315
kester-ng <kester.ng@visenze.com>
2021-04-04 14:38:02 +0800

    Added route repo tests

---
+++ Challo/ChalloTests/TestingUtilities/MockRepositories/MockMapRouteCoreDataRepository.swift
@@ -0,0 +1,46 @@
+//
+//  MockRouteCoreDataRepository.swift
+//  ChalloTests
+//
+//  Created by Kester Ng on 4/4/21.
+//
+
+import CoreData
+import Foundation
+import MapKit
+
+@testable import Challo
+
+class MockMapRouteCoreDataRepository: CoreDataRepository<Route> {
+    typealias mockRepo = MockMapRouteCoreDataRepository
+    
+    static var id = UUID()
+    static var date = Date()
+    
+    static var route1: Route {
+        let route = Route(context: CoreDataContainer.managedObjectContext)
+        route.id = id.uuidString
+        route.comments = "hi"
+        route.date = date
+        route.start = MockMapMarkerCoreDataRepository.marker1
+        route.end = MockMapMarkerCoreDataRepository.marker2
+        return route
+    }
+    
+    static var mapRoute1: MapRoute {
+        MapRoute(id: id,
+                 start: MockMapMarkerCoreDataRepository.mapMarker1,
+                 end: MockMapMarkerCoreDataRepository.mapMarker2,
+                 date: Date(),
+                 comments: "hi")
+    }
+    
+    var data = [Route]()
+    
+    override func getAll() -> [Route] {
+        data
+    }
+    
+    override func commit() {
+    }
+}
