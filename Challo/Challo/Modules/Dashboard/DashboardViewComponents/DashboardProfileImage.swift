f1fa174ede5b77647830ca015860fe83adca8844
Shao Yi <e0310084@u.nus.edu>
2021-04-10 19:50:11 +0800

    Add profileImg to user state

---
+++ Challo/Challo/Modules/Dashboard/DashboardViewComponents/DashboardProfileImage.swift
@@ -0,0 +1,20 @@
+//
+//  DashboardProfileImage.swift
+//  Challo
+//
+//  Created by Shao Yi on 10/4/21.
+//
+
+import SwiftUI
+
+struct DashboardProfileImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DashboardProfileImage_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardProfileImage()
+    }
+}
