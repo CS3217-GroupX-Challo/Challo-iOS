ef026db47ff776df78dcc64af3b3b16549801dc8
Shao Yi <e0310084@u.nus.edu>
2021-04-10 20:11:21 +0800

    Update imageservice as utility class

--- Challo/Challo/Common/Services/ImageService/ImageService.swift
+++ Challo/Challo/Common/Services/ImageService/ImageService.swift
@@ -9,20 +9,20 @@ import SwiftUI
 import ImageKitIO
 import SDWebImageSwiftUI
 
-class ImageService: ImageServiceProtocol {
+struct ImageService: ImageServiceProtocol {    
         
-    private func generateFullUrl(path: String, height: CGFloat, width: CGFloat) -> String {
+    private static func generateFullUrl(path: String, height: CGFloat, width: CGFloat) -> String {
         ImageKit.shared.url(path: path, transformationPosition: .QUERY)
             .height(height: Int(height))
             .aspectRatio(width: Int(width), height: Int(height))
             .create()
     }
     
-    func loadImage(path: String, height: CGFloat = 400, width: CGFloat = 400,
-                   onSuccess: ((UIImage, Data?) -> Void)? = nil) -> some View {
+    static func loadImage(path: String, height: CGFloat = 400, width: CGFloat = 400,
+                          onSuccess: ((UIImage, Data?) -> Void)? = nil) -> Image {
         let fullUrl = generateFullUrl(path: path, height: height, width: width)
         
-        return WebImage(url: URL(string: fullUrl))
+        let image = WebImage(url: URL(string: fullUrl))
             .onSuccess { image, data, _ in
                 onSuccess?(image, data)
             }
@@ -32,10 +32,11 @@ class ImageService: ImageServiceProtocol {
 //            .placeholder {
 //                Rectangle().foregroundColor(.gray)
 //            }
+        return Image(uiImage: image.asUIImage())
     }
         
-    func uploadImage(image: Data, fileName: String, onProgress: ((Progress) -> Void)?,
-                     onSuccess: (() -> Void)?, onFailure: ((Error) -> Void)?) {
+    static func uploadImage(image: Data, fileName: String, onProgress: ((Progress) -> Void)?,
+                            onSuccess: (() -> Void)?, onFailure: ((Error) -> Void)?) {
         ImageKit.shared.uploader().upload(
           file: image,
           fileName: fileName,
--- Challo/Challo/Common/Services/ImageService/ImageServiceProtocol.swift
+++ Challo/Challo/Common/Services/ImageService/ImageServiceProtocol.swift
@@ -10,19 +10,20 @@ import SwiftUI
 protocol ImageServiceProtocol {
     associatedtype SomeImage: View
     
-    func loadImage(path: String, height: CGFloat, width: CGFloat, onSuccess: ((UIImage, Data?) -> Void)?) -> SomeImage
-    func uploadImage(image: Data, fileName: String, onProgress: ((Progress) -> Void)?,
-                     onSuccess: (() -> Void)?, onFailure: ((Error) -> Void)?)
+    static func loadImage(path: String, height: CGFloat, width: CGFloat,
+                          onSuccess: ((UIImage, Data?) -> Void)?) -> SomeImage
+    static func uploadImage(image: Data, fileName: String, onProgress: ((Progress) -> Void)?,
+                            onSuccess: (() -> Void)?, onFailure: ((Error) -> Void)?)
 }
 
 extension ImageServiceProtocol {
-    func loadImage(path: String, height: CGFloat = 400, width: CGFloat = 400,
-                   onSuccess: ((UIImage, Data?) -> Void)? = nil) -> SomeImage {
+    static func loadImage(path: String, height: CGFloat = 400, width: CGFloat = 400,
+                          onSuccess: ((UIImage, Data?) -> Void)? = nil) -> SomeImage {
         loadImage(path: path, height: height, width: width, onSuccess: onSuccess)
     }
     
-    func uploadImage(image: Data, fileName: String, onProgress: ((Progress) -> Void)? = nil,
-                     onSuccess: (() -> Void)? = nil, onFailure: ((Error) -> Void)? = nil) {
+    static func uploadImage(image: Data, fileName: String, onProgress: ((Progress) -> Void)? = nil,
+                            onSuccess: (() -> Void)? = nil, onFailure: ((Error) -> Void)? = nil) {
         uploadImage(image: image, fileName: fileName, onProgress: onProgress,
                     onSuccess: onSuccess, onFailure: onFailure)
     }
