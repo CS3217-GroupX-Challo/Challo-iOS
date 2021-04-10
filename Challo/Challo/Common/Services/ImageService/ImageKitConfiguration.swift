23853a1da00ccdc0c0fa785df52e1cd3d7801bf2
Shao Yi <e0310084@u.nus.edu>
2021-04-10 18:48:43 +0800

    Add imagekit authEndpoint

--- Challo/Challo/Common/Services/ImageService/ImageKitConfiguration.swift
+++ Challo/Challo/Common/Services/ImageService/ImageKitConfiguration.swift
@@ -17,9 +17,14 @@ struct ImageKitConfiguration: ThirdPartyAPIConfiguration {
             fatalError("ImageKit urlEndpoint is not in env var")
         }
         
+        guard let apiPath = ProcessInfo.processInfo.environment["api_path"] else {
+            fatalError("api_path is not in env var")
+        }
+        
         _ = ImageKit(publicKey: publicKey,
                      urlEndpoint: urlEndpoint,
-                     transformationPosition: .PATH)
+                     transformationPosition: .PATH,
+                     authenticationEndpoint: "\(apiPath)/imageKitAUth")
         ChalloLogger.logger.info("ImageKit has been initialised!")
     }
     
