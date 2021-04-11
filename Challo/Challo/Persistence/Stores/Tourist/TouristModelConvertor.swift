06e235d5fb207f254b92bb976cf3533a8491330e
kester-ng <kester.ng@visenze.com>
2021-04-12 00:21:13 +0800

    Added tourist store

---
+++ Challo/Challo/Persistence/Stores/Tourist/TouristModelConvertor.swift
@@ -0,0 +1,28 @@
+//
+//  TouristModelConvertor.swift
+//  Challo
+//
+//  Created by Kester Ng on 12/4/21.
+//
+
+struct TouristModelConvertor {
+    func convertTouristToTouristPersistenceObject(tourist: Tourist) -> TouristPersistenceObject {
+        TouristPersistenceObject(userId: tourist.userId,
+                                 email: tourist.email,
+                                 profileImg: tourist.profileImg,
+                                 name: tourist.name,
+                                 phone: tourist.phone,
+                                 dateJoined: tourist.dateJoined,
+                                 sex: tourist.sex)
+    }
+    
+    func convertTouristPersistenceObjectToTourist(touristObject: TouristPersistenceObject) -> Tourist {
+        Tourist(userId: touristObject.userId,
+                email: touristObject.email,
+                profileImg: touristObject.profileImg,
+                name: touristObject.name,
+                phone: touristObject.phone,
+                dateJoined: touristObject.dateJoined,
+                sex: touristObject.sex)
+    }
+}
---
+++ Challo/Challo/Persistence/Stores/Tourist/TouristStore.swift
@@ -0,0 +1,30 @@
+//
+//  TouristStore.swift
+//  Challo
+//
+//  Created by Kester Ng on 12/4/21.
+//
+
+class TouristStore {
+    private var touristRepository: TouristDetailsRepositoryProtocol
+    private var convertor = TouristModelConvertor()
+    
+    init(touristRepository: TouristDetailsRepositoryProtocol) {
+        self.touristRepository = touristRepository
+    }
+    
+    func getAllTourists() -> [Tourist] {
+        let touristObjects = touristRepository.getAllTourists()
+        return touristObjects.map { touristObject in
+            convertor.convertTouristPersistenceObjectToTourist(touristObject: touristObject)
+        }
+    }
+    
+    func saveTourists(tourists: [Tourist]) {
+        let touristObjects = tourists.map { tourist in
+            convertor.convertTouristToTouristPersistenceObject(tourist: tourist)
+        }
+        
+        touristRepository.saveTourists(touristObjects: touristObjects)
+    }
+}
