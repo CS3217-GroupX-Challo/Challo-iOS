938e94de7ce0a81accaa20c6dbf1d599b5075b62
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2021-04-12 13:20:16 +0800

    Set up Review viper classes

---
+++ Challo/Challo/Modules/Review/ReviewInteractor.swift
@@ -0,0 +1,11 @@
+//
+//  ReviewInteractor.swift
+//  Challo
+//
+//  Created by Tan Le Yang on 12/4/21.
+//
+
+class ReviewInteractor: InteractorProtocol {
+
+    weak var presenter: ReviewPresenter!
+}
---
+++ Challo/Challo/Modules/Review/ReviewPresenter.swift
@@ -0,0 +1,12 @@
+//
+//  ReviewPresenter.swift
+//  Challo
+//
+//  Created by Tan Le Yang on 12/4/21.
+//
+
+class ReviewPresenter: PresenterProtocol {
+
+    var interactor: ReviewInteractor!
+    var router: ReviewRouter?
+}
