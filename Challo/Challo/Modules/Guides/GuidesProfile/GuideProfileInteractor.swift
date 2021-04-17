ee6683808ef973becaf29b0e524f114971f99b8f
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2021-04-18 06:10:58 +0800

    Final fixes

--- Challo/Challo/Modules/Guides/GuidesProfile/GuideProfileInteractor.swift
+++ Challo/Challo/Modules/Guides/GuidesProfile/GuideProfileInteractor.swift
@@ -14,12 +14,23 @@ class GuideProfileInteractor: InteractorProtocol {
     }
     
     func getReviews() {
+        let areaConvertor = AreaModelConvertor()
+        let trailConvertor = TrailModelConvertor(convertor: areaConvertor)
+        let guideConvertor = GuideModelConvertor(areaModelConvertor: areaConvertor,
+                                                 trailModelConvertor: trailConvertor)
+        let reviewConvertor = ReviewModelConvertor(guideModelConvertor: guideConvertor,
+                                                   touristModelConvertor: TouristModelConvertor(),
+                                                   trailModelConvertor: trailConvertor)
+        let reviewStore = ReviewStore(repository: AnyPersistenceRepoProtocol(ReviewDetailsRepository()),
+                                      convertor: reviewConvertor)
         reviewAPI.getReviewsForGuide(guideId: presenter.guide.userId) { [weak self] reviews in
             guard let self = self else {
                 return
             }
             
             self.presenter.reviews = reviews
+            reviewStore.save(models: reviews)
+            let localReviews = reviewStore.getAll()
         }
     }
 }
