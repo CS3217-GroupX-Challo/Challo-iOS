af5df816cd9065b49bd84354705c1cd005d5f3a6
kester-ng <kester.ng@visenze.com>
2021-04-17 21:48:24 +0800

    Removed commments

--- Challo/Challo/Persistence/Stores/Booking/BookingModelConvertor.swift
+++ Challo/Challo/Persistence/Stores/Booking/BookingModelConvertor.swift
@@ -5,65 +5,6 @@
 //  Created by Kester Ng on 13/4/21.
 //
 
-/*
-class BookingModelConvertor {
-    private let trailModelConvertor: TrailModelConvertor
-    private let guideModelConvertor: GuideModelConvertor
-    private let touristModelConvertor: TouristModelConvertor
-    private let reviewModelConvertor: ReviewModelConvertor
-    
-    init(trailModelConvertor: TrailModelConvertor,
-         guideModelConvertor: GuideModelConvertor,
-         touristModelConvertor: TouristModelConvertor,
-         reviewModelConvertor: ReviewModelConvertor) {
-        self.trailModelConvertor = trailModelConvertor
-        self.guideModelConvertor = guideModelConvertor
-        self.reviewModelConvertor = reviewModelConvertor
-        self.touristModelConvertor = touristModelConvertor
-    }
-    
-    func convertBookingToBookingPersistenceObject(booking: Booking) -> BookingPersistenceObject {
-        let trailObject
-            = trailModelConvertor.convertTrailToTrailPersistenceObject(trail: booking.trail)
-        let guideObject
-            = guideModelConvertor.convertGuideToGuidePersistenceObject(guide: booking.guide)
-        let touristObject
-            = touristModelConvertor.convertTouristToTouristPersistenceObject(tourist: booking.tourist)
-        
-        var reviewObject: ReviewPersistenceObject?
-        if let review = booking.review {
-            reviewObject
-                = reviewModelConvertor.convertReviewToReviewPersistenceObject(review: review)
-        }
-        
-        return BookingPersistenceObject(bookingId: booking.bookingId, fee: booking.fee,
-                                        date: booking.date, createdAt: booking.createdAt,
-                                        status: booking.status, guide: guideObject,
-                                        trail: trailObject, tourist: touristObject,
-                                        review: reviewObject)
-    }
-    
-    func convertBookingPersistenceObjectToBooking(bookingObject: BookingPersistenceObject) -> Booking {
-        let trail
-            = trailModelConvertor.convertTrailPersistenceObjectToTrail(trailObject: bookingObject.trail)
-        let guide
-            = guideModelConvertor.convertGuidePersistenceObject(guideObject: bookingObject.guide)
-        let tourist
-            = touristModelConvertor.convertTouristPersistenceObjectToTourist(touristObject:
-                                                                                bookingObject.tourist)
-        var review: Review?
-        if let reviewObject = bookingObject.review {
-            review = reviewModelConvertor.convertReviewPersistenceObjectToReview(reviewObject: reviewObject)
-        }
-        
-        return Booking(bookingId: bookingObject.bookingId, fee: bookingObject.fee,
-                       date: bookingObject.date, createdAt: bookingObject.createdAt,
-                       status: bookingObject.status, guide: guide,
-                       trail: trail, tourist: tourist,
-                       review: review)
-    }
-}*/
-
 class BookingModelConvertor: ModelConvertor {
     typealias Model = Booking
     typealias PersistenceObject = BookingPersistenceObject
--- Challo/Challo/Persistence/Stores/Guide/GuideModelConvertor.swift
+++ Challo/Challo/Persistence/Stores/Guide/GuideModelConvertor.swift
@@ -5,63 +5,6 @@
 //  Created by Kester Ng on 13/4/21.
 //
 
-/*
-class GuideModelConvertor {
-    private let areaModelConvertor: AreaModelConvertor
-    private let trailModelConvertor: TrailModelConvertor
-    
-    init(areaModelConvertor: AreaModelConvertor,
-         trailModelConvertor: TrailModelConvertor) {
-        self.areaModelConvertor = areaModelConvertor
-        self.trailModelConvertor = trailModelConvertor
-    }
-    
-    func convertGuideToGuidePersistenceObject(guide: Guide) -> GuidePersistenceObject {
-        let trailObjects = guide.trails.map { trail in
-            trailModelConvertor.convertTrailToTrailPersistenceObject(trail: trail)
-        }
-        
-        var areaObject: AreaPersistenceObject?
-        if let location = guide.location {
-            areaObject = areaModelConvertor.convertAreaToAreaPersistenceObject(area: location)
-        }
-        
-        return GuidePersistenceObject(userId: guide.userId, email: guide.email,
-                                      profileImg: guide.profileImg,
-                                      name: guide.name, phone: guide.phone,
-                                      dateJoined: guide.dateJoined, location: areaObject,
-                                      sex: guide.sex, rating: guide.rating,
-                                      daysAvailable: guide.daysAvailable, trails: trailObjects,
-                                      unavailableDates: guide.unavailableDates,
-                                      yearsOfExperience: guide.yearsOfExperience,
-                                      languages: guide.languages, accreditations: guide.accreditations,
-                                      biography: guide.biography, hobbies: guide.hobbies,
-                                      memorableExperiences: guide.memorableExperiences)
-    }
-    
-    func convertGuidePersistenceObject(guideObject: GuidePersistenceObject) -> Guide {
-        let trails = guideObject.trails.map { trailObject in
-            trailModelConvertor.convertTrailPersistenceObjectToTrail(trailObject: trailObject)
-        }
-        
-        var location: Area?
-        if let area = guideObject.location {
-            location = areaModelConvertor.convertAreaPersistenceObjectToArea(areaObject: area)
-        }
-        
-        return Guide(userId: guideObject.userId, email: guideObject.email,
-                     profileImg: guideObject.profileImg, name: guideObject.name,
-                     phone: guideObject.phone, dateJoined: guideObject.dateJoined,
-                     location: location, sex: guideObject.sex,
-                     rating: guideObject.rating, daysAvailable: guideObject.daysAvailable,
-                     trails: trails, unavailableDates: guideObject.unavailableDates,
-                     yearsOfExperience: guideObject.yearsOfExperience, languages: guideObject.languages,
-                     accreditations: guideObject.accreditations, biography: guideObject.biography,
-                     hobbies: guideObject.hobbies, memorableExperiences: guideObject.memorableExperiences)
-    }
-}
-*/
-
 class GuideModelConvertor: ModelConvertor {
     typealias Model = Guide
     typealias PersistenceObject = GuidePersistenceObject
