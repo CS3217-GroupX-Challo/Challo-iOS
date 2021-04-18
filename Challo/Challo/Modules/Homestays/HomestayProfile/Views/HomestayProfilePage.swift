bcd68a3b2fdfe433f692e5b7ac9c6dec85a07ed7
Shao Yi <e0310084@u.nus.edu>
2021-04-18 10:50:27 +0800

    Restructure stickybar

--- Challo/Challo/Modules/Homestays/HomestayProfile/Views/HomestayProfilePage.swift
+++ Challo/Challo/Modules/Homestays/HomestayProfile/Views/HomestayProfilePage.swift
@@ -10,9 +10,18 @@ import SwiftUI
 struct HomestayProfilePage: View {
     @EnvironmentObject var presenter: HomestayProfilePresenter
     
+    func makeStickyBarContent(_ homestay: Homestay) -> some View {
+        Text("\(homestay.fee)").bold() + Text(" Rp / night")
+    }
+    
+    var ctaButtonContent: some View {
+        Text("Chat with Host")
+    }
+    
     var body: some View {
         EntityProfilePage<HomestayProfilePresenter>(defaultImage: "guides-background",
                                                     detailsContent: { AnyView(TrailProfileDetails()) },
-                                                    stickyBarContent: { AnyView(TrailProfileStickyBar()) })
+                                                    ctaButtonContent: { AnyView(ctaButtonContent) },
+                                                    stickyBarContent: { trail in AnyView(makeStickyBarContent(trail)) })
     }
 }
